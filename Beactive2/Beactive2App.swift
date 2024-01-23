17388810f71c695e7473eb1869a9896734a498bb
odhis101 <joshodhiambo5@gmail.com>
2024-01-23 10:07:58 +0300

    Initial Commit

---
+++ Beactive2/Beactive2App.swift
@@ -0,0 +1,17 @@
+//
+//  Beactive2App.swift
+//  Beactive2
+//
+//  Created by Joshua on 1/23/24.
+//
+
+import SwiftUI
+
+@main
+struct Beactive2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
